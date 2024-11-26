dd9e9720df3f062d77f4318323291e3261ab5965
takeshii17 <20228299@psu.palawan.edu.ph>
2024-11-26 22:04:22 +0800

    successful chart setup

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -7,3 +7,14 @@ class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
+    
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
